a645b0979a6717bbe1e975c6d28b2678fcc949de
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2013-07-08 10:45:39 +0200

    added display

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 # deploy to tomcat installed via brew
-target=/usr/local/Cellar/tomcat/7.0.37/libexec/webapps
+target=/usr/local/Cellar/tomcat/7.0.41/libexec/webapps
 package=CurrencyConverter-1.0-SNAPSHOT
 
+ function display {
+ 	echo ""
+    echo "* $1"
+    echo ""
+}
+
+
+display "Stopping tomcat"
 catalina stop
+
+display "Intalling new package"
 rm -rf  $target/$package/
+rm $target/*.war
 cp -v ./target/$package.war $target
+
+display "Starting tomcat"
 catalina start
+
+display "done."
