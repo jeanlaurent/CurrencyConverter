32771b9cb9ef019e4093cbcb2d0d46519cb5dcbd
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2013-09-11 12:21:38 +0200

    Adding script to enhance new box creation

--- newbox.sh
+++ newbox.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-vagrant box remove precise64 virtualbox
-vagrant box add precise64 ~/boxes/precise64.box
-vagrant up precise64
+vagrant box remove prod virtualbox
+vagrant box remove factory virtualbox
+
+vagrant box add prod ~/boxes/precise32.box
+vagrant box add factory ~/boxes/factory.box
+
+vagrant up 
