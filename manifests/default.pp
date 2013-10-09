19a05a311f436c0500c34c7e2670a6c3aa36727e
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2013-10-09 10:59:42 +0200

    Setting http proxy through puppet

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,3 +1,17 @@
+$user = "***internet-username***"
+$pass = "***internet-password***"
+$proxy = "http://${user}:${pass}@proxy.fr.world.socgen:8080/"
+file { "/etc/environment":
+  content => "HTTP_PROXY=${proxy}
+              HTTPS_PROXY=${proxy}
+              NO_PROXY=localhost,127.0.0.1"
+}
+$aptconf = 'Acquire { Retries "0"; HTTP { Proxy "$proxy"; }; };
+            Acquire { Retries "0"; HTTPS { Proxy "$proxy"; }; };'
+notify { "$aptconf": }
+#file { "/etc/apt/apt.conf"
+#  content => "$aptconf";
+#}
 exec { "aptitude update":
   path => "/usr/bin",
 }
